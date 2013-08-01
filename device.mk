ae005ecc9b2cf85fb22d7f665d463c03a81de5ab
PeterCxy <xqsx43cxy@126.com>
2013-08-01 12:43:14 +0800

    Fix build

--- device.mk
+++ device.mk
@@ -254,4 +254,4 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # This is the aries-specific audio package
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
+$(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
