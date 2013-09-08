5b423401a2eac7323490cf86a43aa8c76a00c66d
PeterCxy <xqsx43cxy@126.com>
2013-09-08 14:30:29 +0800

    aries: clean up

--- device.mk
+++ device.mk
@@ -151,9 +151,6 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_TAGS += dalvik.gc.type-precise
 
 PRODUCT_PACKAGES += \
-    Updater
-
-PRODUCT_PACKAGES += \
     librs_jni \
     com.android.future.usb.accessory
 
