84a8d843e2188df8008646e81893047eee036362
martincz <martincz@mokeedev.com>
2014-06-24 21:27:38 +0800

    set mi2 c
    Change-Id: Iac82b2202dde0acd69d6855720b6f8fa7618cc11

--- init/init_aries.c
+++ init/init_aries.c
@@ -98,7 +98,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     /* Unknown */
     else {
-        property_set("ro.product.model", "MI 2?");
+        property_set("ro.product.model", "MI 2C");
     }
 
     property_set("ro.product.device", "aries");
