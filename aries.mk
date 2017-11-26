f3f5f6bd0b18ea351de54e02eaeb313f9bc97efd
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-26 15:58:30 +0800

    aries: Import media config from msm8960-common
    Change-Id: I47712e64cc2ec31c54647c03c91cf6297c4ba767

--- aries.mk
+++ aries.mk
@@ -43,6 +43,15 @@ PRODUCT_COPY_FILES += \
 # Light
 PRODUCT_PACKAGES += \
     lights.msm8960
+    
+# Media
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/rootdir/media/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/rootdir/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/rootdir/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
