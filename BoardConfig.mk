b8df4ed1af656f24d48641cbde5c5d8b3ad29117
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-28 17:51:49 +0800

    aries: Set BOARD_KERNEL_IMAGE_NAME
    Change-Id: I79fc6c77b4b666f9caab8114896cbe98d9c71607

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,7 @@ BOARD_KERNEL_BASE    := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lge.kcal=0|0|0|x
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_ARCH := arm
