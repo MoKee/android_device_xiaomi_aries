9bd14c9aea2e730447aea58cbce2ced208d78077
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-28 13:16:20 +0800

    aries: Replace vendor/cm with vendor/lineage
    Change-Id: I1023e7fb4bcb98c0acac84b3ecc7a94b7ffb3fc6

--- lineage.mk
+++ lineage.mk
@@ -8,8 +8,8 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/aries/aries.mk)
