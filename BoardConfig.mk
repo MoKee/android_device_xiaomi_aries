89c8114c68ee801b521f6629a67c35290bcf59d4
GuaiYiHu <guaiyihu@foxmail.com>
2017-11-29 17:00:59 +0800

    aries: Change kernel path to kernel/xiaomi/aries
    Change-Id: I46715bdc5c19c8c371df9a79e9a66add502e707e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,7 +120,7 @@ BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=aries lpj=67677 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lge.kcal=0|0|0|x
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 TARGET_KERNEL_ARCH := arm
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8960
+TARGET_KERNEL_SOURCE := kernel/xiaomi/aries
 TARGET_KERNEL_CONFIG := aries-perf-user_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
