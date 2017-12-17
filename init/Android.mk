e251e4ec1799544b8b9e943da16dd6eb9b43396a
GuaiYiHu <guaiyihu@foxmail.com>
2017-12-17 22:17:25 +0800

    aries: libinit: Include selinux/label.h
    Change-Id: I598669cc13885e4405380f2d8a5bf5f998a28b22

--- init/Android.mk
+++ init/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := system/core/init
+LOCAL_C_INCLUDES := system/core/init external/selinux/libselinux/include
 LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_msm8960.cpp
 LOCAL_MODULE := libinit_msm8960
