1edd66d0026e15a6c55a21d570bd1033cc179ec9
GuaiYiHu <guaiyihu@foxmail.com>
2017-12-23 23:20:12 +0800

    aries: Remove deperated libstlport.
    Change-Id: I7f50f86c5ef70413826bfe918bdb91d11f3a67cb

--- aries.mk
+++ aries.mk
@@ -377,9 +377,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
-PRODUCT_PACKAGES += \
-    libstlport
-
 # Omx
 PRODUCT_PACKAGES += \
     libOmxAacEnc \
