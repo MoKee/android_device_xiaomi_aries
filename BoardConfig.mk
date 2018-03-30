4efb7a59973c923b0644e2b0a04ac4dee5760605
GuaiYiHu <guaiyihu@foxmail.com>
2018-03-30 23:45:58 +0800

    aries: Add f2fs support
    Change-Id: Ie2029dd0e97872687d3b3b173e4a3ac73cd1c6f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ TARGET_RECOVERY_FSTAB            := $(LOCAL_PATH)/rootdir/ramdisk/fstab.aries
 TARGET_RECOVERY_PIXEL_FORMAT     := "RGBX_8888"
 BOARD_HAS_NO_SELECT_BUTTON       := true
 TARGET_USERIMAGES_USE_EXT4       := true
+TARGET_USERIMAGES_USE_F2FS       := true
 
 # Security
 BOARD_USES_SECURE_SERVICES := true
