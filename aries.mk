b109fb5bea45672fcb3e092f8950145d26626462
GuaiYiHu <guaiyihu@foxmail.com>
2018-03-31 23:25:25 +0800

    aries: Switch to AOSP GPS blobs
    Change-Id: I37aab516e5dfa3a38d3701253ea12d1939d704c8

--- aries.mk
+++ aries.mk
@@ -295,9 +295,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.vold.umsdirtyratio=50
 
+# GPS
+PRODUCT_PACKAGES += \
+    libloc_eng \
+    libloc_core \
+    libgps.utils \
+    gps.msm8960
+
 # Gps
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.gps.qmienabled=true
+    persist.gps.qmienabled=true \
+    persist.gps.qc_nlp_in_use=0 \
+    ro.qc.sdk.izat.premium_enabled=0 \
+    ro.qc.sdk.izat.service_mask=0x4 \
+    ro.gps.agps_provider=1
 
 # GNSS HAL
 PRODUCT_PACKAGES += \
